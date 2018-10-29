ff0ea0ab97666e1463d3fc8a20203a76b413a469
RasyadiAbdoellah <rfabdoellah@gmail.com>
2018-10-29 16:26:15 -0400

    create Recipe, IngredientList, IngredientItem components

---
+++ src/containers/Recipe.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+export default class Recipe extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      Recipe: {
+        name: '',
+        details: '',
+        day: 0,
+        week: 0,
+        month: 0,
+        Ingredients:[]
+      }
+    }
+  }
+
+  recipeInputHandler =  (event) => {
+    /*
+    sets recipe state via inputs. Takes the input ID (Recipe-name or Recipe-details) and spilts it.
+    Uses the split results and input value to determine state key:value pair.
+    */
+    const elID = event.target.id.split('-')
+    const objKey = elID[1]
+    const value = event.target.value
+    const Recipe = this.state.Recipe
+
+    Recipe[objKey] = value
+  
+    this.setState({Recipe})
+
+    console.log({Recipe})
+  
+  }
+
+  ingredientHandler = (data) => {
+    /* 
+    handler to take ingredient data and pass it to state.  
+    State obj is hard-coded, so no need to make anything dynamic.
+    Requires data sent up to be an array.
+    */
+
+    this.setState({Recipe:{Ingredients: data}})
+  }
+
+render() {
+  return (
+    <div>
+      <form>
+        <label>
+          Recipe Name:
+          <input id="Recipe-name" value={this.state.Recipe.name} onChange={this.recipeInputHandler} />
+        </label>
+
+        <label>
+          Details:
+          <textarea id="Recipe-details" value={this.state.Recipe.details} onChange={this.recipeInputHandler}/>
+        </label>
+
+      </form>
+    </div>
+  )
+}
+}
