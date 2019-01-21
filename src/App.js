0d05f397821bf7e9b22d9805f9668cd0b24071ff
RasyadiAbdoellah <rfabdoellah@gmail.com>
2019-01-20 22:18:04 -0500

    Move Dashboard code to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-import {Dashboard} from './components';
+import {RecipeForm, RecipeList} from './components';
 
 class App extends Component {
-  render() {
+  constructor(props){
+    super()
+    this.state = {
+        displayRecipeList: true,
+        displayRecipeForm: false,
+        
+    }
+}
+
+toggleRecipeList = () => {
+    this.setState({
+        displayRecipeList: !this.state.displayRecipeList
+    })
+}
+
+toggleRecipeForm = () => {
+    this.setState({
+        displayRecipeForm: !this.state.displayRecipeForm
+    })
+}
+
+render(){
     return (
-      <div className="App">
-        <Dashboard />
-      </div>
-    );
-  }
+        <div className="App">
+            <button onClick={this.toggleRecipeList}> Recipe List</button>
+            <button onClick ={this.toggleRecipeForm}> Add Recipe </button>
+            {this.state.displayRecipeForm && <RecipeForm/>}
+            {this.state.displayRecipeList && <RecipeList/>}
+        </div>
+    )
+}
 }
 
 export default App;
