92c1c5649842d58719ae4cdb87ce91bb7bf3cb58
RasyadiAbdoellah <rfabdoellah@gmail.com>
2019-05-03 19:51:29 -0400

    Add comments

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import history from './history';
 import Auth from './Auth/Auth';
 import Callback from './components/Callback';
 
+// Goal for auth is to have a custom landing page that stops ppl from using the app if they're not signed in.
+// on sign in it redirect to /home or /main or /dash.
+// on sign out it redirects back to /
+// signed in  users get autoredirected to /home or whatever if they navigate to /
+// TODOS:
+// figure out how to make custom signin widget and prevent initial redirect
+// figure out how to tie in auth status to redux
+// figure out routing logic
+
 const auth = new Auth();
 
 const handleAuthentication = ({ location }) => {
