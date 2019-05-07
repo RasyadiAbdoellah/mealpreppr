801299ae67560526001f3faa30f7396140ec36fb
Ras Abdoellah <rfabdoellah@gmail.com>
2019-05-07 12:27:28 -0400

    fix setSession in Auth setSession this was not set to parent but to itself. Changed all class methods to arrow funcs to remove the need to .bind(this) in constructor.

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,55 +1,45 @@
-import auth0 from 'auth0-js';
-import history from '../history';
+import auth0 from "auth0-js";
+import history from "../history";
 export default class Auth {
   auth0 = new auth0.WebAuth({
-    domain: 'mealprepr.auth0.com',
-    clientID: 'Om5Q7N90qxCMySOfW9pTr2nix8PjbEHs',
-    redirectUri: 'http://localhost:3000/callback',
-    responseType: 'token id_token',
-    scope: 'openid',
+    domain: "mealprepr.auth0.com",
+    clientID: "Om5Q7N90qxCMySOfW9pTr2nix8PjbEHs",
+    redirectUri: "http://localhost:3000/callback",
+    responseType: "token id_token",
+    scope: "openid"
   });
 
   accessToken;
   idToken;
   expiresAt;
 
-  constructor() {
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
-    this.handleAuthentication = this.handleAuthentication.bind(this);
-    this.isAuthenticated = this.isAuthenticated.bind(this);
-    this.getAccessToken = this.getAccessToken.bind(this);
-    this.getIdToken = this.getIdToken.bind(this);
-    this.renewSession = this.renewSession.bind(this);
-  }
-
-  login() {
+  login = () => {
     this.auth0.authorize();
-  }
+  };
 
-  handleAuthentication() {
+  handleAuthentication = () => {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
       } else if (err) {
-        history.replace('/');
+        history.replace("/");
         console.log(err);
         alert(`Error: ${err.error}. Check the console for further details.`);
       }
     });
-  }
+  };
 
-  getAccessToken() {
+  getAccessToken = () => {
     return this.accessToken;
-  }
+  };
 
-  getIdToken() {
+  getIdToken = () => {
     return this.idToken;
-  }
+  };
 
-  setSession(authResult) {
+  setSession = authResult => {
     // Set isLoggedIn flag in localStorage
-    localStorage.setItem('isLoggedIn', 'true');
+    localStorage.setItem("isLoggedIn", "true");
     console.log(authResult);
     // Set the time that the access token will expire at
     let expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
@@ -58,10 +48,10 @@ export default class Auth {
     this.expiresAt = expiresAt;
 
     // navigate to the home route
-    history.replace('/');
-  }
+    history.replace("/");
+  };
 
-  renewSession() {
+  renewSession = () => {
     this.auth0.checkSession({}, (err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
@@ -69,33 +59,33 @@ export default class Auth {
         this.logout();
         console.log(err);
         alert(
-          `Could not get a new token (${err.error}: ${err.error_description}).`,
+          `Could not get a new token (${err.error}: ${err.error_description}).`
         );
       }
     });
-  }
+  };
 
-  logout() {
+  logout = () => {
     // Remove tokens and expiry time
     this.accessToken = null;
     this.idToken = null;
     this.expiresAt = 0;
 
     // Remove isLoggedIn flag from localStorage
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
 
     this.auth0.logout({
-      return_to: window.location.origin,
+      return_to: window.location.origin
     });
 
     // navigate to the home route
-    history.replace('/');
-  }
+    history.replace("/");
+  };
 
-  isAuthenticated() {
+  isAuthenticated = () => {
     // Check whether the current time is past the
     // access token's expiry time
     let expiresAt = this.expiresAt;
     return new Date().getTime() < expiresAt;
-  }
+  };
 }
